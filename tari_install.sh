2634c27db794c98e54fcc9c75d382849900590df
fxpy <58055522+fxpy@users.noreply.github.com>
2021-06-01 23:28:20 +0300

    Update tari_install.sh
    fixed

--- tari_install.sh
+++ tari_install.sh
@@ -2,15 +2,14 @@
 #
 #Tari Node installer
 
-mkdir $HOME/tari && cd $HOME/tari
-
-wget -O tari.zip wget https://tari-binaries.s3.amazonaws.com/current/linux/tari_base_node-ubuntu-18.04-x64-0.8.9-a07c1a3-release.zip
-
 sudo apt update && sudo apt upgrade -y
 sudo apt install git curl build-essential cmake clang pkg-config libssl-dev libsqlite3-dev sqlite3 unzip screen -y
 
-unzip tari_base_node-ubuntu-18.04-x64-0.8.9-a07c1a3-release.zip
-rm tari_base_node-ubuntu-18.04-x64-0.8.9-a07c1a3-release.zip
+mkdir $HOME/tari && cd $HOME/tari
+wget -O tari.zip https://tari-binaries.s3.amazonaws.com/current/linux/tari_base_node-ubuntu-18.04-x64-0.8.9-a07c1a3-release.zip
+
+unzip tari.zip
+rm tari.zip
 
 sudo ./install_tor.sh
 sudo ./setup_tor_service
