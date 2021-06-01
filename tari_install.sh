474d3de8f26f7996fa81aff8d078523a64f44a27
fxpy <58055522+fxpy@users.noreply.github.com>
2021-06-01 23:50:19 +0300

    Update tari_install.sh
    fixed

--- tari_install.sh
+++ tari_install.sh
@@ -8,9 +8,10 @@ sudo apt install git curl build-essential cmake clang pkg-config libssl-dev libs
 mkdir $HOME/tari && cd $HOME/tari
 wget -O tari.zip https://tari-binaries.s3.amazonaws.com/current/linux/tari_base_node-ubuntu-18.04-x64-0.8.9-a07c1a3-release.zip
 
-unzip tari.zip
-rm tari.zip
+unzip $HOME/tari/tari.zip && rm $HOME/tari/tari.zip
 
-sudo ./install_tor.sh
-sudo ./setup_tor_service
-sudo ./tari_base_node --create-id -y -y
+sudo $HOME/tari/install_tor.sh
+sudo $HOME/tari/setup_tor_service
+sudo $HOME/tari/tari_base_node --create-id
+
+screen -S node $HOME/tari/tari_base_node
