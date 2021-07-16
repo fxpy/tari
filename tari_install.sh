bab3ee9b9efe3fe4df514855d09e73e03f528156
fxpy <58055522+fxpy@users.noreply.github.com>
2021-07-16 09:28:47 +0300

    Update tari_install.sh
    update to 0.8.10

--- tari_install.sh
+++ tari_install.sh
@@ -6,12 +6,14 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install git curl build-essential cmake clang pkg-config libssl-dev libsqlite3-dev sqlite3 unzip screen -y
 
 mkdir $HOME/tari && cd $HOME/tari
-wget -O tari.zip https://tari-binaries.s3.amazonaws.com/current/linux/tari_base_node-ubuntu-18.04-x64-0.8.9-a07c1a3-release.zip
+wget -O tari.zip https://tari-binaries.s3.amazonaws.com/latest/linux/tari_base_node-ubuntu-18.04-x64-0.8.10-65fc65a-release.zip
 
 unzip $HOME/tari/tari.zip && rm $HOME/tari/tari.zip
 
 sudo $HOME/tari/install_tor.sh
 sudo $HOME/tari/setup_tor_service
 sudo $HOME/tari/tari_base_node --create-id
+
+rm -rf $HOME/.tari/stibbons/db
 
 screen -S node $HOME/tari/tari_base_node
